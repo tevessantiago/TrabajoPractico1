9493db6ddfb8aeddd3c08e5c341aa6c266d3fcd9
Santitevess <39637063+Santitevess@users.noreply.github.com>
2018-05-27 15:46:41 -0300

    Add files via upload

---
+++ TP1TEVES.c
@@ -0,0 +1,75 @@
+/* Una empresa textil maneja sus ventas por medio de corredores que cobran  comisiones sobre el total de las mismas.
+Tiene 4 artículos y son 10 corredores con las siguientes comisiones:
+-	corredor 1,2,3 =   5 %
+-	corredor 4,5,6 =   8 %
+-	corredor 7,8,9 = 10 %
+-	corredor 10    = 12 %
+el precio de sus artículos son 
+-	art 1 = $10
+-	art 2 = $15
+-	art 3 = $20
+-	art 4 = $12
+cada vez que realiza una venta sabe 
+-	nro de corredor
+-	nro de artículo
+-	cantidad
+-	descuento
+Calcular e imprimir
+a.	importe total vendido por cada corredor
+b.	porcentual que representa sobre la venta de la empresa
+c.	comisión a cobrar por cada corredor
+d.	cantidad de ventas realizadas por cada corredor
+e.	promedio del valor vendido por cada corredor
+f.	nro de corredor que vendió más
+*/
+
+/*#include <stdio.h>
+
+double comision123 = 5;
+double comision456 = 8;
+double comision789 = 10;
+double comision10 = 12;
+int precioArticulo1 = 10;
+int precioArticulo2 = 15;
+int precioArticulo3 = 20;
+int precioArticulo4 = 12;
+
+int TomarDatos();
+int CalcularVentaDeCorredor(int, int, int, int);
+int CalcularVenta(int, int, int);
+
+int main()
+{
+	int NumerodeCorredor, ArticuloVendido, CantidadArticulosVendidos;
+	double DescuentoAplicado;
+	
+	printf("Ingrese numero de corredor o ingrese 0 para terminar\n");
+	scanf("%d",&NumerodeCorredor);
+	while(NumerodeCorredor != 0)
+	{
+		printf("Ingrese numero del articulo vendido\n");
+		scanf("%d",&ArticuloVendido);
+		printf("Ingrese cantidad de articulos vendidos\n");
+		scanf("%d",&CantidadArticulosVendidos);
+		printf("Ingrese cantidad de descuento aplicado\n");
+		scanf("%f",&DescuentoAplicado);
+		printf("Ingrese numero de corredor o ingrese 0 para terminar\n");
+		scanf("%d",&NumerodeCorredor);
+	}
+	return 0;
+}
+
+CalcularVentaDeCorredor(Corredor, Articulo, Cantidad, Descuento)
+{
+	switch(Corredor)
+	{
+		case 1 ... 3: CalcularVenta(Articulo, Cantidad, Descuento); break;
+		case 4 ... 6: CalcularVenta(Articulo, Cantidad, Descuento); break;
+		case 7 ... 9: CalcularVenta(Articulo, Cantidad, Descuento); break;
+		case 	  10: CalcularVenta(Articulo, Cantidad, Descuento); break;	
+		
+	}
+	return 0;
+}
+
+CalcularVenta(Articulo, Cantidad, Descuento)*/
